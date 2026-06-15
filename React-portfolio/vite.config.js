c4bd3c5a775be6c866b3b12ab686b601e8b6734a
cjw04 <93749597+cjw0417@users.noreply.github.com>
2026-06-16 01:11:25 +0900

    0616 수정
    0616 수정

--- React-portfolio/vite.config.js
+++ React-portfolio/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Jae-s_Portfolio/',
+  base: '/React_portfolio/',
 })
